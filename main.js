b4d6390e05716c08e90bb9cad68108bb8c9badd8
amirhossein khanmohammadi <ak47.amirhosseinkhanmohamadi@gmail.com>
2025-12-04 00:56:25 +0330

    function add server worker impeliment

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
- 'use strict'
-    
-    let info = null;
-    const form = document.querySelector('.form');
-    
-    const dinamic=document.querySelector('.dinamic')
-    
+'use strict'
 
-    async function getApi() {
+let info = null;
+const form = document.querySelector('.form');
 
-        const url='https://foodish-api.com/api/images'
+const dinamic = document.querySelector('.dinamic')
+
+
+async function installServiceWorkerAsync() {
+    if ('serviceWorker' in navigator) {
         try {
+            let serviceWorker = await navigator.serviceWorker.register('/sw.js')
+            console.log(`Service worker registered ${serviceWorker}`)
+        } catch (err) {
+            console.error(`Failed to register service worker: ${err}`)
+        }
+    }
+}
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.onload = function () {
+        installServiceWorkerAsync()
+    }
+})
 
-            const res = await fetch(`${url}/${info}`);
 
-            if (!res.ok) {
-                console.log('error')
-                throw new Error("error ocuured");
-                
-            }
+async function getApi() {
 
-            const data = await res.json();
-            dinamic.insertAdjacentHTML('afterbegin',
+    const url = 'https://foodish-api.com/api/images'
+    try {
+
+        const res = await fetch(`${url}/${info}`);
+
+        if (!res.ok) {
+            console.log('error')
+            throw new Error("error ocuured");
+
+        }
+
+        const data = await res.json();
+        dinamic.insertAdjacentHTML('afterbegin',
             `
             <div class='image-wrapper'>
                 
@@ -29,20 +46,20 @@
             </div>
             
             `)
-            
 
-        } catch (error) {
 
-            console.log(error)
+    } catch (error) {
 
-        }
+        console.log(error)
+
     }
+}
 
-    form.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
 
-        e.preventDefault();
-        const input=e.target[0]
-        info = input.value;
-        getApi()
+    e.preventDefault();
+    const input = e.target[0]
+    info = input.value;
+    getApi()
 
-    })
+})
